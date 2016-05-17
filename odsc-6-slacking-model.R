2ded50179bea2a584abd1773085e288d29836f35
Krzysztof Sakrejda <krzysztof@gmail.com>
2016-05-17 11:29:04 -0400

    Added a simulation of a random effect outlier, basic comments on the outcome on the estimates using the NC model, likely the centered parameterization would fare even worse in terms of convergence at least (could check).  Next do odsc-7 that re-fits the t-distributed model to this.  Rest of the work now is laying out parameter comparisons in more detail...

---
+++ odsc-6-slacking-model.R
@@ -0,0 +1,71 @@
+## Still talking about widgets and the hierarchical model, but touch on
+## non-centered parameterization.  Talk about parameterizations.
+## Show funnel vs. inverted funnel.
+
+## Using the original simulated batch data, we run a slightly
+## modified Stan program to estimate team effects.
+m9 <- stan('m6-estimate-nc-hierarchy.stan', 
+  data=list(N=N, K=K, M=M, 
+    y=slacking_data[['y']], 
+    widget=slacking_data[['widget']],
+    batch=slacking_data[['batch']], 
+    team=slacking_data[['team']], 
+    batch_team_index=slacking_batch_team_map[['team']],
+    widget_batch_index=slacking_widget_batch_map[['batch']]), 
+    chains=5, iter=200+1000, warmup=1000)
+
+s9 <- rstan::extract(m9)
+
+e9 <- s9[['team_effects']] %>% data.frame(check.names=FALSE) %>% 
+  mutate(iteration=1:nrow(.)) %>% gather(team, quality, -iteration) %>% 
+  group_by(team) %>% summarise(
+    `team_10%`=quantile(quality,.1), 
+     team_mean=mean(quality), 
+    `team_90%`=quantile(quality,.9)) %>% 
+  mutate(team_f = factor(team), team=as.numeric(team))
+
+## Plot estimates:
+#  x-axis: simulated value.
+#  y-axis: data set index (there should be M of them).
+#  red dots: data set mean.
+#  blue line: true mean.
+pl <- ggplot() + geom_point(
+  data=left_join(slacking_data, e9, c('team','team_f')), 
+  aes(x=y, y=batch_f, color=team_f), shape=2
+) + geom_point(
+  data=slacking_batch_stats, 
+  aes(x=mean, y=batch_f, group=team_f), color="red", size=3
+) + geom_errorbarh(
+  data=right_join(e9, slacking_batch_stats, c('team','team_f')),
+  aes(xmin=`team_10%`, x=team_mean, xmax=`team_90%`, 
+      y=batch_f, group=team_f), color='blue'
+) + geom_point(
+  data=right_join(e9, slacking_batch_stats, c('team','team_f')),
+  aes(x=team_mean, y=batch_f), color='blue'
+) + geom_vline(xintercept=mu, color="blue") + 
+    theme_minimal() + 
+    facet_wrap( ~ team_f, ncol=1, drop=TRUE, 
+      shrink=TRUE, scales='free_y')
+
+
+stop("STOPPING HERE ON PURPOSE.")
+
+# Find team we have the least data on:
+slacking_data %>% group_by(team) %>% summarise(n=n()) %>% 
+  ungroup() %>% arrange(as.numeric(team))
+
+# Find estimate of sigma vs. team_sigma, show
+# the semi-funnel (bivariate plot, x-axis: team_effects[1],
+# y-axis team_sigma, then show how semi-funnel disappears
+# for team_effects[1], but reappears inverted for team_effects_raw[2]...
+#
+# The main point here is to show the "ALL team_effects" estimates and
+# point out that the non-slacking teams effects are over-estimated, 
+# the standard deviation parameter is bogus (huge), and the mean is
+# also uncertain.
+launch_shinystan(m9)
+
+print(pl)
+
+# Estimate of sqrt(sigma^2 + team_sigma^2) should be total 
+# sigma from simulation.
---
+++ odsc-6-slacking-simulation.R
@@ -0,0 +1,95 @@
+## Old scenario and a single model for multiple batches: 
+
+## Scenario: some sort of quality control situation where
+## we're manufacturing widgets and each widget is scored 
+## on quality after manufacturing.  Widgets are manufactured
+## in batches and each batch is produced by a single 
+## manufacturing team.
+##
+## Data: We would like to evaluate the teams on the quality of their
+## manufacturing over time and also track particularly low/high
+## quality batches, so in addition to the quality score for
+## each widget we record: 
+##         1) the quality score,
+##         2) the widget id, 
+##         3) the batch id, and 
+##         4) the team id
+
+## I'm calling this a pre-hierarchical model because
+## it has all the right structure but we don't use
+## that in the estimation.
+
+library(rstan); library(shinystan)
+library(magrittr); library(dplyr); library(tidyr)
+
+# We are simulating data for M batches of N quality measurements
+# with a mean of mu and standard deviation of sigma from the 
+# normal distribution. Any given batch can be produced by any
+# of K teams
+K <- 4
+M <- 30  
+N <- 15
+mu <- 3.52
+sigma <- 7.11
+batch_sigma <- 3
+team_effects <- seq(from=-2, to=2, length.out=K-1) %>%
+  c(-50)
+team_weights <- ((1:K)^2) / sum((1:K)^2)
+
+
+## Again, simulate from this model:
+m8 <- stan('m3-simulate-batches.stan', data=list(
+  N=N, K=K, M=M, mu=mu, sigma=sigma, batch_sigma=batch_sigma,
+  team_effects=team_effects,
+  team_weights=team_weights),
+  iter=100+500, warmup=500, chains=1)
+
+s8 <- rstan::extract(m8)
+
+## Assign batch to team, with uneven probability.
+slacking_batch_team_map <- data.frame(
+  batch=1:M, 
+  team=s8[['batch_team_index']][100,,drop=TRUE]
+)
+
+## Assign widget to batch:
+slacking_widget_batch_map <- data.frame(
+  widget=1:(N*M),
+  batch=s8[['widget_batch_index']][100,,drop=TRUE]
+)
+
+## Assemble the data set:
+slacking_data <- data.frame(
+  y=s8[['y']][100,,drop=TRUE],
+  widget=1:(N*M)) %>%
+  left_join(slacking_widget_batch_map, by='widget') %>% 
+  left_join(slacking_batch_team_map, by='batch') %>%
+  mutate(batch_f=factor(batch), team_f = factor(team))
+
+## Calculate batch and team statistics.
+slacking_batch_stats <- slacking_data %>% group_by(team, batch) %>% 
+  summarise(mean=mean(y), sigma=sd(y)) %>% ungroup %>%
+  mutate(batch_f=factor(batch), team_f=factor(team)) %>%
+  arrange(batch)
+
+slacking_team_stats <- slacking_data %>% group_by(team) %>%
+  summarise(mean=mean(y), sigma=sd(y), se=sd(y)/sqrt(n())) %>% ungroup %>%
+  mutate(team_f=factor(team))
+
+## Plot summaries:
+#  x-axis: simulated value.
+#  y-axis: data set index (there should be M of them).
+#  red dots: data set mean.
+#  blue line: true mean.
+#  color: team index.
+pl <- ggplot(data=slacking_data, 
+  aes(x=y, y=batch, color=factor(team))) + 
+  geom_point(shape=2) + 
+  geom_point(data=slacking_batch_stats, aes(x=mean, y=batch), 
+    color="red", size=3) +
+  geom_vline(xintercept=mu, color="blue") + theme_minimal() +
+  facet_wrap( ~ team_f, ncol=1)
+
+
+print(pl)
+
